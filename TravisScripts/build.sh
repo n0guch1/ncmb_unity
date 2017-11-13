3292c5e559e78f8d8fa59245428ab3879400d72d
n0guch1 <noguchi.yutaka@stf.nifty.co.jp>
2017-11-13 13:26:32 +0900

    test14

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -26,10 +26,13 @@ cat $log_file
 test_result_file=$(pwd)/TravisScripts/test_runner_result.xml
 echo "* Execute Test Runner"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+-batchmode \
+-nographics \
 -runTests \
 -projectPath "$project_path" \
 -testResults "$test_result_file" \
 -testPlatform editmode
+-quit
 
 echo '* Test Runner result'
 cat $test_result_file 
