5b55964905eacc3d650c5e860c1561b52223a6cd
n0guch1 <noguchi.yutaka@stf.nifty.co.jp>
2017-11-15 11:43:50 +0900

    test31

--- ncmb_unity/Assets/PlayModeTest/NCMBTestSettings.cs
+++ ncmb_unity/Assets/PlayModeTest/NCMBTestSettings.cs
@@ -10,10 +10,8 @@ public class NCMBTestSettings
 	public static readonly string APP_KEY = "d55786e0bd4263018ca04ac9af7cbad153d757879d8cd296a9c3075a482ad845";
 	public static readonly string CLIENT_KEY = "ba82f3ca775edf2c39b53bc7ecaa40eb2a7dd35f4724f489b067ec93895e21de";
 	public static readonly string DOMAIN_URL = "http://localhost:3000";
-	//public static readonly string DOMAIN_URL = "";
 	public static readonly string API_VERSION = "2013-09-01";
 	private static bool _callbackFlag = false;
-	private static readonly int REQUEST_TIME_OUT = 10;
 
 	public static bool CallbackFlag {
 		get {
@@ -55,19 +53,10 @@ public class NCMBTestSettings
 
 	public static IEnumerator AwaitAsync ()
 	{
-		Debug.Log ("確認3");
-		float elapsedTime = 0.0f;
 		while (NCMBTestSettings.CallbackFlag == false) {
-			Debug.Log ("確認4:" + elapsedTime);
-			elapsedTime += 0.5f;
-			if (elapsedTime >= REQUEST_TIME_OUT) { 
-				Debug.Log ("確認　Break！！");
-				yield break;
-			}
 			//yield return new WaitForEndOfFrame ();
-			yield return new WaitForSeconds (0.5f); 
+			yield return new WaitForSeconds (0.2f); 
 		}
-		Debug.Log ("確認5");
 		yield break;
 	}
 }
