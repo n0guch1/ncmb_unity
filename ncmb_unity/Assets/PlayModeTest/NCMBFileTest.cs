4f1ae826dfc725cdcdf6665d8292871631f5cb8f
n0guch1 <noguchi.yutaka@stf.nifty.co.jp>
2017-11-15 17:45:54 +0900

    test34

--- ncmb_unity/Assets/PlayModeTest/NCMBFileTest.cs
+++ ncmb_unity/Assets/PlayModeTest/NCMBFileTest.cs
@@ -89,7 +89,7 @@ public class NCMBFileTest
 	[UnityTest]
 	public IEnumerator FileUploadImageTest ()
 	{
-		FileStream fileStream = new FileStream ("Assets/Editor/logo.png", FileMode.Open, FileAccess.Read);
+		FileStream fileStream = new FileStream ("Assets/PlayModeTest/logo.png", FileMode.Open, FileAccess.Read);
 		BinaryReader bin = new BinaryReader (fileStream);
 		byte[] data = bin.ReadBytes ((int)bin.BaseStream.Length);
 		bin.Close ();
